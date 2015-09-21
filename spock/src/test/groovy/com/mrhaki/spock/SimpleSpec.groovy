20e7a46da2dc1b284052b87f3bf17b5420537664
gwmccort <gwmccort@gmail.com>
2015-09-21 17:06:18 -0500

    added mocks and stubs returning example

---
+++ spock/src/test/groovy/com/mrhaki/spock/SimpleSpec.groovy
@@ -0,0 +1,38 @@
+package com.mrhaki.spock
+
+//@Grab('org.spockframework:spock-core:1.0-groovy-2.4')
+import spock.lang.Specification
+import spock.lang.Subject
+
+class SampleSpec extends Specification {
+
+    @Subject
+    def util = new StringUtil()
+
+    def "Calculate sizes of String values"() {
+        given:
+        def calculator = Mock(Calculator)
+        util.calculator = calculator
+
+        when:
+        def total = util.size('one', 'two', 'three', 'four', 'five')
+
+        then:
+        5 * calculator.calculateSize(_) >>> [1,3,4]
+
+        total == 1 + 3 + 4 + 4 + 4
+    }
+
+}
+
+class StringUtil {
+    def calculator
+
+    int size(final String[] s) {
+        s.sum { calculator.calculateSize(it) }
+    }
+}
+
+interface Calculator {
+    int calculateSize(final String s)
+}

c1654a20abf7163ee4c23e97332e5605ba536cd1
gwmccort <gwmccort@gmail.com>
2015-10-19 15:58:29 -0500

    geb table w/ page/module

--- geb/src/main/groovy/module/ModuleList.groovy
+++ geb/src/main/groovy/module/ModuleList.groovy
@@ -4,6 +4,9 @@ import geb.Browser
 import geb.Page
 import geb.Module
 
+/**
+ * from: http://adhockery.blogspot.com/2010/11/modelling-repeating-structures-with-geb.html
+ */
 class ModuleList {
 
 
@@ -15,9 +18,17 @@ class ModuleList {
             println "at: " + at(ModuleListPage)
             assert at(ModuleListPage)
 
-
-            println books.recentBooks
+            println '-------booksModule.recentBooks--------'
+            println booksModule.recentBooks
 //            println content
+
+            println '-----booksFromPage--------'
+            println booksFromPage
+
+            println '-----bookResults--------'
+            println bookResults(0).title
+            println bookResults(3).price
+            println bookResults(0)
 
 //            {module Books}.each{println it}
         }
@@ -34,6 +45,17 @@ class Books extends Module {
 //    }
 }
 
+class BookRow extends Module {
+    static content = {
+        cell { i -> $("td", i) }
+        title { cell(0).text() }
+        author { cell(1).text() }
+        format { cell(2).text() }
+        price { cell(3).text()[1..-1].toDouble() }
+        releaseDate { cell(4).text() }
+    }
+}
+
 class ModuleListPage extends Page {
     // url of page
     static url = "file:///H:/Project_Files/bit-bucket/examples/geb/ModuleList.html"
@@ -42,7 +64,11 @@ class ModuleListPage extends Page {
 //    static at = { $('h1', text: contains('Module List')) }
     static at = { title == 'Module List' }
 
-    static content = { books { module Books }}
+    static content = {
+        booksModule { module Books }
+        booksFromPage { $("ol#recent-books li")*.text() }
+        bookResults { i -> module BookRow, $("table#book-results tbody tr", i) }
+    }
 //    static content = { module Books }
 
 }

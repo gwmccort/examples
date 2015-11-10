29a41436d38f0c0b604e55dc82b0d66eecb89ad6
Glen McCort <gwmccort@gmail.com>
2015-11-10 16:43:25 -0600

    guava multi map example

---
+++ guava/src/main/java/com/javarticles/guava/MultimapExample.java
@@ -0,0 +1,45 @@
+package com.javarticles.guava;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+
+/**
+ * Multimap example
+ * from: http://javarticles.com/2015/11/guava-multimap-example.html
+ * 
+ * @author Glen
+ *
+ */
+public class MultimapExample {
+    private static final String COLOR = "Color";
+    private static final String RED = "Red";
+    private static final String YELLOW = "Yellow";
+    private static final String GREEN = "Green";
+    
+    private static final String FURNITURE = "Furniture";
+    private static final String TABLE = "Table";
+    private static final String CHAIR = "Chair";
+    private static final String SOFA = "Sofa";
+    
+    public static void main(String[] args) {
+       Multimap<String,String> bag = ArrayListMultimap.create();
+       bag.put(COLOR, RED);
+       bag.put(COLOR, RED);
+       bag.put(COLOR, YELLOW);
+       bag.put(COLOR, GREEN);
+       bag.put(COLOR, GREEN);
+       bag.put(COLOR, GREEN);
+       
+       bag.put(FURNITURE, TABLE);
+       bag.put(FURNITURE, CHAIR);
+       bag.put(FURNITURE, SOFA);
+
+       System.out.println("Total items in Bag: " + bag.size());
+       System.out.println("Total colors in Bag: " + bag.get(COLOR).size());
+       System.out.println("Colors in Bag: " + bag.get(COLOR));
+       
+       System.out.println("Total items in furniture: " + bag.get(FURNITURE).size());
+       System.out.println("Furniture Items: " + bag.get(FURNITURE));
+    }   
+}

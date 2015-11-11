ca512559872989bbc05939ddbebcdb0e7615f108
Glen McCort <gwmccort@gmail.com>
2015-11-10 19:26:22 -0600

    added junit ignore example

---
+++ java-examples/src/main/java/com/areyes/junit/svc/MessageService.java
@@ -0,0 +1,51 @@
+package com.areyes.junit.svc;
+
+/**
+ * The Class MessageService.
+ * from: http://examples.javacodegeeks.com/core-java/junit/junit-disable-test-example/
+ */
+public class MessageService {
+	
+	
+
+	/** The message. */
+	private String message;
+
+
+	/**
+	 * Instantiates a new message service.
+	 *
+	 * @param message the message
+	 */
+	public MessageService(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Prints the message.
+	 *
+	 * @return the string
+	 */
+	public String printMessage() {
+		return message;
+	}
+
+	/**
+	 * Salutation message.
+	 *
+	 * @return the string
+	 */
+	public String salutationMessage() {
+		message = "Hi!" + message;
+		return message;
+	}
+	
+	/**
+	 * This will be the method to get the salutation messages specifically for executives.
+	 * @return
+	 */
+	public String salutationMessageForExecutives() {
+		return "this is not yet implemented";
+	}
+
+}
---
+++ java-examples/src/test/java/com/areyes/junit/svc/MessageServiceTest.java
@@ -0,0 +1,45 @@
+package com.areyes.junit.svc;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.isA;
+
+/**
+ * MessageServiceTest - disable test example
+ * from: http://examples.javacodegeeks.com/core-java/junit/junit-disable-test-example/
+ * 
+ * @author Glen
+ *
+ */
+public class MessageServiceTest {
+
+	private String CONST_MSG = "Message is me";
+	private MessageService msgService = new MessageService(CONST_MSG);
+	
+	@Test
+	public void testPrintMessage() {
+		//	Check type return
+		assertThat(msgService.printMessage(), isA(String.class));
+		assertEquals(CONST_MSG, msgService.printMessage());
+		
+	}
+	
+	@Test
+	public void testSalutationMessage() {
+		String messageSal = msgService.salutationMessage();
+		assertThat(messageSal, isA(String.class));
+		assertEquals("Hi!" + CONST_MSG,messageSal);
+	}
+
+
+	@Ignore //ignore this test
+	@Test
+	public void testSalutationMessageForExecutives() {
+		assertThat(msgService.salutationMessageForExecutives(), isA(String.class));
+		assertEquals(CONST_MSG, msgService.salutationMessage());
+	}
+}

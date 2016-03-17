67656ffccef24e44671d8ee9ed90dd53745db0a6
Glen McCort <gwmccort@gmail.com>
2016-03-16 23:33:28 -0500

    added tika examples

---
+++ Tika/lucene-example-master/src/main/java/gwm/tika/audio/Mp3Parse.java
@@ -0,0 +1,58 @@
+package gwm.tika.audio;
+
+import java.io.File;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+
+import org.apache.tika.exception.TikaException;
+
+import org.apache.tika.metadata.Metadata;
+
+import org.apache.tika.parser.ParseContext;
+
+import org.apache.tika.parser.mp3.LyricsHandler;
+
+import org.apache.tika.parser.mp3.Mp3Parser;
+
+import org.apache.tika.sax.BodyContentHandler;
+
+import org.xml.sax.SAXException;
+
+/**
+ * @author Glen
+ * @see <a href=
+ *      http://www.tutorialspoint.com/tika/tika_extracting_mp3_files.htm>Extracting
+ *      mp3 Files</a>
+ */
+public class Mp3Parse {
+
+	public static void main(final String[] args) throws Exception, IOException, SAXException, TikaException {
+
+		// detecting the file type
+		BodyContentHandler handler = new BodyContentHandler();
+
+		Metadata metadata = new Metadata();
+		FileInputStream inputstream = new FileInputStream(new File("documents/Sleep Away.mp3"));
+		ParseContext pcontext = new ParseContext();
+
+		// Mp3 parser
+		Mp3Parser Mp3Parser = new Mp3Parser();
+		Mp3Parser.parse(inputstream, handler, metadata, pcontext);
+		LyricsHandler lyrics = new LyricsHandler(inputstream, handler);
+
+		while (lyrics.hasLyrics()) {
+			System.out.println(lyrics.toString());
+		}
+
+		System.out.println("Contents of the document:" + handler.toString());
+		System.out.println("Metadata of the document:");
+
+		String[] metadataNames = metadata.names();
+		for (String name : metadataNames) {
+			System.out.println(name + ": " + metadata.get(name));
+		}
+	}
+
+}

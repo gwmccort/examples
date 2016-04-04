cdce0f83dc079c921bbab610501a3320dbb3b830
Glen McCort <gwmccort@gmail.com>
2016-04-03 23:00:37 -0500

    doc & music

--- LuceneMusicTest/src/main/groovy/Track.groovy
+++ LuceneMusicTest/src/main/groovy/Track.groovy
@@ -34,228 +34,230 @@ import au.com.bytecode.opencsv.CSVWriter
  * @author gwmccort
  *
  */
+//@Slf4j
 @TupleConstructor(force=true)
 @ToString(includeNames=true)
 class Track {
-    String artist
-    String name
-    String album
+	String artist
+	String name
+	String album
 
-    private static final LUCENE_INDEX = 'index'
-    private static final ARTIST_FIELD = 'artist'
-    private static final NAME_FIELD = 'name'
-    private static final ALBUM_FIELD = 'album'
+	private static final LUCENE_INDEX = 'index'
+	private static final ARTIST_FIELD = 'artist'
+	private static final NAME_FIELD = 'name'
+	private static final ALBUM_FIELD = 'album'
 
-    /**
-     * Create Track from music file tags
-     * @param tag mp3 metadata
-     */
-    Track(Tag tag) {
-        artist = tag.getFirst(FieldKey.ARTIST)
-        name=tag.getFirst(FieldKey.TITLE)
-        album=tag.getFirst(FieldKey.ALBUM)
-    }
+	/**
+	 * Create Track from music file tags
+	 * @param tag mp3 metadata
+	 */
+	Track(Tag tag) {
+		artist = tag.getFirst(FieldKey.ARTIST)
+		name=tag.getFirst(FieldKey.TITLE)
+		album=tag.getFirst(FieldKey.ALBUM)
+	}
 
-    /**
-     * Convert track to array of strings
-     * @return String array of track information
-     */
-    String[] toStringArray() {
-        [artist, name, album] as String[]
-    }
+	/**
+	 * Convert track to array of strings
+	 * @return String array of track information
+	 */
+	String[] toStringArray() {
+		[artist, name, album] as String[]
+	}
 
-    static main(args) {
-        println 'starting...'
-        //		indexTest()
-        //		searchTest('test')
-        //		multiSearchTest('test')
+	static main(args) {
+		println 'starting...'
+		//		indexTest()
+		//		searchTest('test')
+		//		multiSearchTest('test')
 
-        def tracks = getMp3Files()
-        println tracks.size()
-        writeTracksToCSV(tracks)
-        indexTracks(tracks)
+//		def tracks = getMp3Files(/P:\FANTOM HDS721010CLA332\Media\Xmas Music/)
+		def tracks = getMp3Files(/P:\FANTOM HDS721010CLA332\Media\Music/)
+		println "tracks size: ${tracks.size()}"
+		writeTracksToCSV(tracks)
+		indexTracks(tracks)
+		println 'end!'
+	}
 
-        //		writeTracksToCSV(getTracks())
-        println 'end!'
-    }
+	/**
+	 * Test adding items to lucene index.
+	 */
+	static void indexTest() {
+		println '>>> indexing tracks...'
 
-    /**
-     * Test adding items to lucene index.
-     */
-    static void indexTest() {
-        println '>>> indexing tracks...'
+		Directory dir = FSDirectory.open(Paths.get(LUCENE_INDEX))
+		Analyzer analyzer = new StandardAnalyzer()
+		IndexWriterConfig iwc = new IndexWriterConfig(analyzer)
 
-        Directory dir = FSDirectory.open(Paths.get(LUCENE_INDEX))
-        Analyzer analyzer = new StandardAnalyzer()
-        IndexWriterConfig iwc = new IndexWriterConfig(analyzer)
+		//		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND)
+		iwc.setOpenMode(OpenMode.CREATE)
+		IndexWriter writer = new IndexWriter(dir, iwc)
 
-        //		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND)
-        iwc.setOpenMode(OpenMode.CREATE)
-        IndexWriter writer = new IndexWriter(dir, iwc)
+		// add track to index
+		def tracks = getSampleTracks()
+		for (track in tracks) {
+			Document doc = new Document()
+			Field artist = new Field(ARTIST_FIELD, track.artist, TextField.TYPE_STORED)
+			Field name = new Field(NAME_FIELD, track.name, TextField.TYPE_STORED)
+			Field album = new Field(ALBUM_FIELD, track.album, TextField.TYPE_STORED)
+			doc.add(artist)
+			doc.add(name)
+			doc.add(album)
+			writer.addDocument(doc)
+		}
 
-        // add track to index
-        def tracks = getSampleTracks()
-        for (track in tracks) {
-            Document doc = new Document()
-            Field artist = new Field(ARTIST_FIELD, track.artist, TextField.TYPE_STORED)
-            Field name = new Field(NAME_FIELD, track.name, TextField.TYPE_STORED)
-            Field album = new Field(ALBUM_FIELD, track.album, TextField.TYPE_STORED)
-            doc.add(artist)
-            doc.add(name)
-            doc.add(album)
-            writer.addDocument(doc)
-        }
+		writer.close()
+	}
 
-        writer.close()
-    }
+	/**
+	 * Index list of tracks to lucene
+	 * @param tracks list of tracks
+	 */
+	static void indexTracks(Track[] tracks) {
+		println '>>> indexing tracks...'
 
-    /**
-     * Index list of tracks to lucene
-     * @param tracks list of tracks
-     */
-    static void indexTracks(Track[] tracks) {
-        println '>>> indexing tracks...'
+		Directory dir = FSDirectory.open(Paths.get(LUCENE_INDEX))
+		Analyzer analyzer = new StandardAnalyzer()
+		IndexWriterConfig iwc = new IndexWriterConfig(analyzer)
 
-        Directory dir = FSDirectory.open(Paths.get(LUCENE_INDEX))
-        Analyzer analyzer = new StandardAnalyzer()
-        IndexWriterConfig iwc = new IndexWriterConfig(analyzer)
+		//		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND)
+		iwc.setOpenMode(OpenMode.CREATE)
+		IndexWriter writer = new IndexWriter(dir, iwc)
 
-        //		iwc.setOpenMode(OpenMode.CREATE_OR_APPEND)
-        iwc.setOpenMode(OpenMode.CREATE)
-        IndexWriter writer = new IndexWriter(dir, iwc)
+		// add track to index
+		//        for (track in sampleTracks) {
+		for (track in tracks) {
+			Document doc = new Document()
+			Field artist = new Field(ARTIST_FIELD, track.artist, TextField.TYPE_STORED)
+			Field name = new Field(NAME_FIELD, track.name, TextField.TYPE_STORED)
+			Field album = new Field(ALBUM_FIELD, track.album, TextField.TYPE_STORED)
+			doc.add(artist)
+			doc.add(name)
+			doc.add(album)
+			writer.addDocument(doc)
+		}
 
-        // add track to index
-        for (track in sampleTracks) {
-            Document doc = new Document()
-            Field artist = new Field(ARTIST_FIELD, track.artist, TextField.TYPE_STORED)
-            Field name = new Field(NAME_FIELD, track.name, TextField.TYPE_STORED)
-            Field album = new Field(ALBUM_FIELD, track.album, TextField.TYPE_STORED)
-            doc.add(artist)
-            doc.add(name)
-            doc.add(album)
-            writer.addDocument(doc)
-        }
+		writer.close()
+	}
 
-        writer.close()
-    }
+	/**
+	 * Search lucene index
+	 * @param queryString lucene query
+	 */
+	static void searchTest(String queryString) {
+		println '>>> searching tracks...'
 
-    /**
-     * Search lucene index
-     * @param queryString lucene query
-     */
-    static void searchTest(String queryString) {
-        println '>>> searching tracks...'
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(LUCENE_INDEX)))
+		IndexSearcher searcher = new IndexSearcher(reader)
+		Analyzer analyzer = new StandardAnalyzer()
 
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(LUCENE_INDEX)))
-        IndexSearcher searcher = new IndexSearcher(reader)
-        Analyzer analyzer = new StandardAnalyzer()
+		QueryParser parser = new QueryParser(NAME_FIELD, analyzer)
+		Query query = parser.parse(queryString)
+		//		println query
+		//		println query.class
 
-        QueryParser parser = new QueryParser(NAME_FIELD, analyzer)
-        Query query = parser.parse(queryString)
-        //		println query
-        //		println query.class
+		TopDocs results = searcher.search(query, 100)
 
-        TopDocs results = searcher.search(query, 100)
+		ScoreDoc[] hits = results.scoreDocs
+		println hits.size()
+		for (sd in hits) {
+			//			println sd
+			//			println sd.dump()
+			Document doc = searcher.doc(sd.doc)
+			println "artist:${doc.get(ARTIST_FIELD)} name:${doc.get(NAME_FIELD)} album:${doc.get(ALBUM_FIELD)}"
+		}
+	}
 
-        ScoreDoc[] hits = results.scoreDocs
-        println hits.size()
-        for (sd in hits) {
-            //			println sd
-            //			println sd.dump()
-            Document doc = searcher.doc(sd.doc)
-            println "artist:${doc.get(ARTIST_FIELD)} name:${doc.get(NAME_FIELD)} album:${doc.get(ALBUM_FIELD)}"
-        }
-    }
+	static multiSearchTest(String queryString) {
+		println '>>> multi field search ...'
 
-    static multiSearchTest(String queryString) {
-        println '>>> multi field search ...'
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(LUCENE_INDEX)))
+		IndexSearcher searcher = new IndexSearcher(reader)
+		Analyzer analyzer = new StandardAnalyzer()
 
-        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(LUCENE_INDEX)))
-        IndexSearcher searcher = new IndexSearcher(reader)
-        Analyzer analyzer = new StandardAnalyzer()
+		String[] fields = [ARTIST_FIELD, NAME_FIELD, ALBUM_FIELD]
+		QueryParser parser = new MultiFieldQueryParser(fields, analyzer)
+		Query query = parser.parse(queryString)
+		//		println query
+		//		println query.class
 
-        String[] fields = [ARTIST_FIELD, NAME_FIELD, ALBUM_FIELD]
-        QueryParser parser = new MultiFieldQueryParser(fields, analyzer)
-        Query query = parser.parse(queryString)
-        //		println query
-        //		println query.class
+		TopDocs results = searcher.search(query, 100)
 
-        TopDocs results = searcher.search(query, 100)
+		ScoreDoc[] hits = results.scoreDocs
+		println hits.size()
+		for (sd in hits) {
+			//			println sd
+			//			println sd.dump()
+			Document doc = searcher.doc(sd.doc)
+			println "artist:${doc.get(ARTIST_FIELD)} name:${doc.get(NAME_FIELD)} album:${doc.get(ALBUM_FIELD)}"
+		}
 
-        ScoreDoc[] hits = results.scoreDocs
-        println hits.size()
-        for (sd in hits) {
-            //			println sd
-            //			println sd.dump()
-            Document doc = searcher.doc(sd.doc)
-            println "artist:${doc.get(ARTIST_FIELD)} name:${doc.get(NAME_FIELD)} album:${doc.get(ALBUM_FIELD)}"
-        }
+	}
 
-    }
+	/**
+	 * Get list of tracks for testing
+	 * @return list of tracks
+	 */
+	static Track[] getSampleTracks() {
+		def tracks = []
+		tracks << new Track(artist:'Yonter Mountain String Band', name:'40 Miles from Denver', album:'Elevation test')
+		tracks << new Track(artist:'Grateful Dead', name:'Trucking test', album:'American Beauty')
+		tracks << new Track(artist:'Test artist', name:'test track name', album:'test album')
+		tracks
+	}
 
-    /**
-     * Get list of tracks for testing
-     * @return list of tracks
-     */
-    static Track[] getSampleTracks() {
-        def tracks = []
-        tracks << new Track(artist:'Yonter Mountain String Band', name:'40 Miles from Denver', album:'Elevation test')
-        tracks << new Track(artist:'Grateful Dead', name:'Trucking test', album:'American Beauty')
-        tracks << new Track(artist:'Test artist', name:'test track name', album:'test album')
-        tracks
-    }
+	/**
+	 * Example of using jaudiotagger to get mp3 tags
+	 * @return list of tracks
+	 */
+	static Track[] getMp3Files(String pathRoot) {
+		println 'in getMp3Files...'
+		def results = []
 
-    /**
-     * Example of using jaudiotagger to get mp3 tags
-     * @return list of tracks
-     */
-    static Track[] getMp3Files(String pathRoot) {
-        println 'in getMp3Files...'
-        def results = []
+		// disable jul logging output
+		java.util.logging.Logger globalLogger = java.util.logging.Logger.getLogger("");
+		Handler[] handlers = globalLogger.getHandlers();
+		for (Handler handler : handlers) {
+			globalLogger.removeHandler(handler);
+		}
 
-        // disable jul logging output
-        java.util.logging.Logger globalLogger = java.util.logging.Logger.getLogger("");
-        Handler[] handlers = globalLogger.getHandlers();
-        for (Handler handler : handlers) {
-            globalLogger.removeHandler(handler);
-        }
-
-        //		new File(/C:\Users\Public\Music/).eachDirRecurse { dir ->
-        //			new File(/C:\Users\Glen\Music/).eachDirRecurse { dir ->
+		//		new File(/C:\Users\Public\Music/).eachDirRecurse { dir ->
+		//			new File(/C:\Users\Glen\Music/).eachDirRecurse { dir ->
 		println "pathRoot: $pathRoot"
-        new File(pathRoot).eachDirRecurse { dir ->
+		new File(pathRoot).eachDirRecurse { dir ->
 			println "dir: $dir"
-            dir.eachFileMatch(~/.*.mp3/) { file ->
+			dir.eachFileMatch(~/.*.mp3/) { file ->
 				println "file: $file"
-                try {
-                    //					MP3File mf = new MP3File(file)
-                    MP3File mf = new MP3File(file, MP3File.LOAD_ALL, true)
-                    Tag tag = mf.getTag()
-                    //					Track t = new Track(artist:tag.getFirst(FieldKey.ARTIST), name:tag.getFirst(FieldKey.TITLE), album:tag.getFirst(FieldKey.ALBUM))
-                    Track t = new Track(tag)
-                    results << t
-                }
-                catch (Exception e) {
-                    //					e.printStackTrace()
-                    println e
-                }
-            }
-        }
-        results
-    }
+				try {
+					//					MP3File mf = new MP3File(file)
+					MP3File mf = new MP3File(file, MP3File.LOAD_ALL, true)
+					Tag tag = mf.getTag()
+					//					Track t = new Track(artist:tag.getFirst(FieldKey.ARTIST), name:tag.getFirst(FieldKey.TITLE), album:tag.getFirst(FieldKey.ALBUM))
+					Track t = new Track(tag)
+					results << t
+				}
+				catch (Exception e) {
+					//					e.printStackTrace()
+					println e
+				}
+			}
+		}
+		results
+	}
 
-    /**
-     * Write tacks to a CSV file
-     * @param tracks
-     */
-    static void writeTracksToCSV(Track[] tracks) {
-        println 'in writeTracksToCSV'
-        CSVWriter writer = new CSVWriter(new FileWriter('Track.csv'))
-        sampleTracks.each { t ->
-            writer.writeNext(t.toStringArray())
-        }
-        writer.close()
-    }
+	/**
+	 * Write tacks to a CSV file
+	 * @param tracks
+	 */
+	static void writeTracksToCSV(Track[] tracks) {
+		println 'in writeTracksToCSV'
+		CSVWriter writer = new CSVWriter(new FileWriter('Track.csv'))
+		//        sampleTracks.each { t ->
+		tracks.each { t ->
+			writer.writeNext(t.toStringArray())
+		}
+		writer.close()
+	}
 
 }
 

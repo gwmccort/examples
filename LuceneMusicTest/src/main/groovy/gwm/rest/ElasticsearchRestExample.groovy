592e5eedaa10c26d9ce9f8e743f63e480b426a54
gwmccort <gwmccort@gmail.com>
2016-05-31 11:06:11 -0500

    es rest example

---
+++ LuceneMusicTest/src/main/groovy/gwm/rest/ElasticsearchRestExample.groovy
@@ -0,0 +1,42 @@
+package gwm.rest
+
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+import groovy.json.JsonOutput
+import groovyx.net.http.HTTPBuilder
+
+
+class ElasticsearchRestExample {
+
+	static main(args) {
+		HTTPBuilder http = new HTTPBuilder('http://192.168.99.100:9200')
+
+		http.get(path: '/') { resp, json ->
+			println resp.status
+			println json
+			println JsonOutput.prettyPrint(JsonOutput.toJson(json))
+			println "cluster_name: ${json.cluster_name}"
+		}
+
+
+		http.request(POST, JSON) {
+			uri.path = '/music/_search'
+			body = [
+				query : [
+					match : [
+						name : 'track'
+					]
+
+				]
+			]
+
+			response.success = { resp, json ->
+				println resp.status
+				println json
+				println JsonOutput.prettyPrint(JsonOutput.toJson(json))
+				println "hits: ${json.hits}"
+			}
+		}
+	}
+
+}

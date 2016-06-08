5f5c4fed4c12de188a4b4843963c863df97425bc
Glen McCort <gwmccort@gmail.com>
2016-06-08 12:33:49 -0500

    doc & kt

--- kotlin/src/main/kotlin/MainApp.kt
+++ kotlin/src/main/kotlin/MainApp.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 /**
  * My first test of kotlin
  *
@@ -7,6 +9,9 @@
 fun main(args: Array<String>) {
     println("Hello Kotlin")
     println("sum(1,2): ${sum(1, 2)}")
+
+    // reads all the lines from a file and prints them out
+    File("build.gradle").forEachLine { println(it) }
 }
 
 fun sum(a: Int, b: Int): Int {
